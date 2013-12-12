fc513b7f86fdb70e84554f957b86b99b752174d8
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-12-12 16:04:57 -0300

    Se agrega orden y filtro por fecha para promos

--- application/models/promo_model.php
+++ application/models/promo_model.php
@@ -11,10 +11,8 @@ class Promo_model extends CI_Model {
 
     public function get_promos($limit, $offset)
     {
-        $this->db->select('*')
-        ->from('promos')
-        ->limit($limit,$offset);
-        return $this->db->get()->result_array();       
+        $query = $this->db->query('select t1.* from promos as t1 where t1.start_datetime <= now() and t1.end_datetime > now() order by start_datetime desc limit '.$offset.','.$limit );
+        return $query->result_array();     
     }
 
 }
