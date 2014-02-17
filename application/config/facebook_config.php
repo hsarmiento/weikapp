fd990274962203319d3a931c3bbaa71bce1c9d65
cbaspree <sebastian.bravot@gmail.com>
2014-02-17 19:23:38 -0300

    semi arregla layout

--- application/config/facebook_config.php
+++ application/config/facebook_config.php
@@ -2,8 +2,8 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['appId'] = '187057784828315';
-$config['appSecret'] = '243f86f6ee9620f5d8ecc9183df294fd';
-$config['accessToken'] = '187057784828315|kgZefqYOC65oP5gTn5Fvn5k2EDc';
+$config['appId'] = '536439783130946';
+$config['appSecret'] = 'f1e1d359c5d5b9aa874673ebc637d3bf';
+$config['accessToken'] = '536439783130946|ChOdE2u5lAxoaY8NKgev9K4dAeI';
 
 ?>
