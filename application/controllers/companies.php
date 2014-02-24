2fdda2f73bf8028fc46afe051cf086c40cd0ba81
cbaspree <sebastian.bravot@gmail.com>
2014-02-24 17:10:49 -0300

    guarda y redimensiona fotos de empresas creadas por el owner, no traidas desde facebook

--- application/controllers/companies.php
+++ application/controllers/companies.php
@@ -19,6 +19,7 @@ class Companies extends CI_Controller
 	{
 		logged_or_redirect('owners/authenticate', 'owners/profile');
 		$this->layout->js(array(base_url().'public/js/jquery.validate.min.js'));
+		$this->layout->css(array(base_url().'public/css/owners.css'));
 		$this->layout->view('add');
 	}
 
@@ -29,10 +30,32 @@ class Companies extends CI_Controller
 		$this->company_model->initialize($this->session->userdata('uid'),null,$this->input->post('name'),$this->input->post('city'));
 		$this->company_model->save();
 		$aResult = $this->company_model->get_fields_with_limits('id,name',array('owner_id' => $this->session->userdata('uid')),1,0,'id desc');
-		
+		echo '<pre>';
+		print_r($_FILES);
+		echo '</pre>';
 		// upload image		
-		imagepng(imagecreatefromstring(file_get_contents($_FILES['company_logo']['tmp_name'])), './public/img/companies/'.md5($aResult[0]['id']).'.png');
-
+		imagepng(imagecreatefromstring(file_get_contents($_FILES['company_logo']['tmp_name'])), './public/img/companies_orig/'.md5($aResult[0]['id']).'.png');
+		// resize image
+		$this->load->library('image_lib');
+		$config= array(
+			'source_image' => './public/img/companies_orig/'.md5($aResult[0]['id']).'.png',
+            'new_image' => './public/img/companies_orig/'.md5($aResult[0]['id']).'.png',
+            // 'maintain_ratio' => TRUE,
+            'width' => 80,
+            'height' => 80
+        );
+        $this->image_lib->initialize($config);
+    	$this->image_lib->resize();
+    	$this->image_lib->clear();
+    	$config= array(
+			'source_image' => './public/img/companies_orig/'.md5($aResult[0]['id']).'.png',
+            'new_image' => './public/img/companies_small/'.md5($aResult[0]['id']).'.png',
+            // 'maintain_ratio' => TRUE,
+            'width' => 32,
+            'height' => 32
+        );
+        $this->image_lib->initialize($config);
+    	$this->image_lib->resize();
 		//set new company as default
 		$this->session->set_userdata('company_id', $aResult[0]['id']);
 		$this->session->set_userdata('company_name', $aResult[0]['name']);
@@ -41,7 +64,7 @@ class Companies extends CI_Controller
 		$this->subscription_model->initialize(1,$aResult[0]['id']);
 		$this->subscription_model->save();
 		//redirect to profile
-		redirect(base_url().'owners/profile');
+		// redirect(base_url().'owners/profile');
 				
 	}
 }
