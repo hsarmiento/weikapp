cb534d98886d2acf5087e048938581b0715acc85
hsarmiento <hernan.sarmientoa@gmail.com>
2014-02-26 16:03:31 -0300

    Se agrega carga de tooltip a layout

--- application/views/layouts/layout.php
+++ application/views/layouts/layout.php
@@ -89,5 +89,23 @@
 				<?php }
 			?>
 		</footer>
+		<script>
+		  $(function() {
+		    $( document ).tooltip({
+		      position: {
+		        my: "center bottom-20",
+		        at: "center top",
+		        using: function( position, feedback ) {
+		          $( this ).css( position );
+		          $( "<div>" )
+		            .addClass( "arrow" )
+		            .addClass( feedback.vertical )
+		            .addClass( feedback.horizontal )
+		            .appendTo( this );
+		        }
+		      }
+		    });
+		  });
+  </script>
 	</body>
 </html>
